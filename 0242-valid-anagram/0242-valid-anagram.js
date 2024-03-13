5a66e80ef93c855c0dc8e2905e619407e950f6b1
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-03-13 15:20:03 +0530

    Time: 68 ms (70.15%), Space: 55.2 MB (10.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,12 +4,40 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
- let sArr = s.split('');
+    // let sArr = s.split('');
+    // let tArr = t.split('');
+    // let sortedS = sArr.sort();
+    // let sortedT = tArr.sort();
+    // if (sortedS.join('') === sortedT.join('')) {
+    //     return true
+    // }
+    // return false;
+    
+    
+    let sArr = s.split('');
     let tArr = t.split('');
-    let sortedS = sArr.sort();
-    let sortedT = tArr.sort();
-    if (sortedS.join('') === sortedT.join('')) {
-        return true
+
+    let map = new Map();
+
+    sArr.forEach((i) => {
+        if (!map.has(i)) {
+            map.set(i, 1);
+        } else {
+            map.set(i, map.get(i) + 1);
+        }
+    })
+
+
+    for (let i in tArr) {
+        if (map.has(tArr[i])) {
+            if (map.get(tArr[i]) === 1) {
+                map.delete(tArr[i]);
+            } else {
+                map.set(tArr[i], map.get(tArr[i]) - 1);
+            }
+        }else {
+            return false;
+        }
     }
-    return false;
+    return map.size === 0;
 };
