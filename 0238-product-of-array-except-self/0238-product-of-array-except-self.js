36dbbaaae60b3cfb22bcfc5471e70556144dffd8
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-03-14 15:35:25 +0530

    Time: 94 ms (35.10%), Space: 59.6 MB (75.91%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -33,31 +33,55 @@ var productExceptSelf = function(nums) {
 //     return result;
     
     
+//     let n = nums.length;
+//     let suffixArr = Array(n).fill(0);
+    
+//     suffixArr[n-1] = nums[n-1];
+    
+    
+//     for(let i = n-2;i>=0;i--){
+//         suffixArr[i] = nums[i]*suffixArr[i+1];
+//     }
+//     let result = Array(n).fill(0);
+    
+//     let productTillNow = 1;
+    
+//     for(let i = 0;i<n;i++){
+//         if(i===0){
+//             result[0] = suffixArr[i+1];
+//         }
+//         else if(i===n-1){
+//             result[n-1] = productTillNow;
+//         }else{
+//             result[i] = productTillNow*suffixArr[i+1];
+//         }
+//         productTillNow = productTillNow*nums[i];
+//     }
+//     return result;   
+    
+    
     let n = nums.length;
-    let suffixArr = Array(n).fill(0);
-    
-    suffixArr[n-1] = nums[n-1];
-    
-    
-    for(let i = n-2;i>=0;i--){
-        suffixArr[i] = nums[i]*suffixArr[i+1];
-    }
     let result = Array(n).fill(0);
+    result[0] = nums[0];
+    
+    for(let i=1;i<n;i++){
+        result[i] = nums[i]*result[i-1];
+    }
     
     let productTillNow = 1;
     
-    for(let i = 0;i<n;i++){
+    for(let i = n-1;i>=0;i--){
         if(i===0){
-            result[0] = suffixArr[i+1];
-        }
-        else if(i===n-1){
-            result[n-1] = productTillNow;
+            result[0] = productTillNow;
+        }else if(i===n-1){
+            result[n-1] = result[n-2];
         }else{
-            result[i] = productTillNow*suffixArr[i+1];
+            result[i] = productTillNow * result[i-1];
         }
-        productTillNow = productTillNow*nums[i];
+        productTillNow *= nums[i];
     }
-    return result;   
+    return result;
+    
 };
 
 
