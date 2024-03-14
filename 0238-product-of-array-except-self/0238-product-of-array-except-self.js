af02e90a6e15cd331abcf3f26aa304e9f997ac74
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-03-14 15:18:04 +0530

    Time: 86 ms (63.27%), Space: 59.1 MB (84.90%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,32 +3,72 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
+//     let n = nums.length;
+//     let prefixArr = Array(n).fill(0);
+//     let suffixArr = Array(n).fill(0);
+    
+//     prefixArr[0] = nums[0];
+//     suffixArr[n-1] = nums[n-1];
+    
+//     for(let i = 1;i<n;i++){
+//         prefixArr[i] = nums[i]*prefixArr[i-1];
+//     }
+    
+//     for(let i = n-2;i>=0;i--){
+//         suffixArr[i] = nums[i]*suffixArr[i+1];
+//     }
+    
+//     let result = Array(n).fill(0);
+    
+//     for(let i = 0;i<n;i++){
+//         if(i===0){
+//             result[0] = suffixArr[i+1];
+//         }
+//         else if(i===n-1){
+//             result[n-1] = prefixArr[i-1];
+//         }else{
+//             result[i] = suffixArr[i+1]*prefixArr[i-1];
+//         }
+//     }
+//     return result;
+    
+    
     let n = nums.length;
-    let prefixArr = Array(n).fill(0);
     let suffixArr = Array(n).fill(0);
     
-    prefixArr[0] = nums[0];
     suffixArr[n-1] = nums[n-1];
     
-    for(let i = 1;i<n;i++){
-        prefixArr[i] = nums[i]*prefixArr[i-1];
-    }
     
     for(let i = n-2;i>=0;i--){
         suffixArr[i] = nums[i]*suffixArr[i+1];
     }
-    
     let result = Array(n).fill(0);
+    
+    let productTillNow = 1;
     
     for(let i = 0;i<n;i++){
         if(i===0){
             result[0] = suffixArr[i+1];
         }
         else if(i===n-1){
-            result[n-1] = prefixArr[i-1];
+            result[n-1] = productTillNow;
         }else{
-            result[i] = suffixArr[i+1]*prefixArr[i-1];
+            result[i] = productTillNow*suffixArr[i+1];
         }
+        productTillNow = productTillNow*nums[i];
     }
-    return result;
+    return result;   
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
