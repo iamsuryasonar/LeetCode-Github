1455d59f7242efb3e30252ef6c6e62cf8eeed859
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-03-21 19:13:13 +0530

    Time: 58 ms (54.34%), Space: 52.4 MB (9.54%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+    
+    if(s.length===1){
+        return false;
+    }
+    
+    for(i in s){
+        if(s[i]==='('|| s[i]==='{'|| s[i]==='['){
+            stack.push(s[i])
+        }else if(s[i]===')'){
+            if(stack[stack.length-1]==='('){
+                stack.pop()
+            }else return false
+        }else if(s[i]==='}'){
+            if(stack[stack.length-1]==='{'){
+                stack.pop()
+            }else return false
+        }else if(s[i]===']'){
+                if(stack[stack.length-1]==='['){
+                stack.pop()
+            }else return false
+        }
+    }
+    
+    return stack.length===0
+};
