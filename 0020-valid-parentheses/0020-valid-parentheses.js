aa2565e4da5dfefb5ae3592e5b9989ea0df6a3c9
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-03-21 19:14:02 +0530

    Time: 59 ms (48.61%), Space: 52.3 MB (9.54%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -5,10 +5,6 @@
 var isValid = function(s) {
     let stack = [];
     
-//     if(s.length===1){
-//         return false;
-//     }
-    
     for(i in s){
         if(s[i]==='('|| s[i]==='{'|| s[i]==='['){
             stack.push(s[i])
