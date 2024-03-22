0543057ccbff6669e282d30ca382b296291a47c0
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-03-22 16:13:58 +0530

    Time: 63 ms (7.14%), Space: 48.6 MB (80.24%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function(x, y) {
+    let xBit = x.toString(2);
+    let yBit = y.toString(2);
+    
+    if(xBit.length > yBit.length){
+        yBit = yBit.padStart(xBit.length, "0");
+    } else if(yBit.length > xBit.length){
+        xBit = xBit.padStart(yBit.length, "0");
+    }
+    
+    let count = 0;
+    
+    for(let i=0;i<xBit.length;i++){
+        if(xBit[i]!==yBit[i]){
+            count = count+1;
+        }
+    }
+    return count;
+    
+};
