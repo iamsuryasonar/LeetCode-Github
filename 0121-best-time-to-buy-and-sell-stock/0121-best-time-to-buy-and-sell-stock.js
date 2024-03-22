c022dda223d78e417a463e50d910f27309084304
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-03-23 00:58:44 +0530

    Time: 69 ms (80.19%), Space: 58.9 MB (63.00%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -4,6 +4,7 @@
  */
 var maxProfit = function(prices) {
     // Brute force approach
+    
     // let max = Number.MIN_VALUE;
     // for(let i=0;i<prices.length ; i++){
     //  for(let j=i;j<prices.length ; j++){
@@ -12,21 +13,33 @@ var maxProfit = function(prices) {
     // }
     // return max;
     
+    // using space
+    // let max = Number.MIN_VALUE;
+    // let suffix = Array(prices.length).fill(0);
+
+    // for(let i=prices.length-1;i>=0;i--){
+    //     if(i === prices.length-1){
+    //         suffix[i] = prices[i]
+    //     }else{
+    //         suffix[i] = Math.max(prices[i],suffix[i+1])
+    //     }
+    // }
+
+    // for(let i=0; i<prices.length; i++){
+    //     max = Math.max(max,suffix[i]-prices[i])
+    // }
+
+    // return max;
+    
     let max = Number.MIN_VALUE;
-    let suffix = Array(prices.length).fill(0);
-    
-    for(let i=prices.length-1;i>=0;i--){
-        if(i === prices.length-1){
-            suffix[i] = prices[i]
-        }else{
-            suffix[i] = Math.max(prices[i],suffix[i+1])
-        }
-    }
-    
+    let minSoFar = prices[0];
+
     for(let i=0; i<prices.length; i++){
-        max = Math.max(max,suffix[i]-prices[i])
+        minSoFar = Math.min(minSoFar,prices[i])
+        max = Math.max(prices[i]-minSoFar,max)
     }
-    
+
     return max;
     
+
 };
