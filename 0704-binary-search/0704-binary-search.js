148625565a12f6563d0059aef4a5da159092b39b
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-03-23 00:30:38 +0530

    Time: 58 ms (46.66%), Space: 52.8 MB (8.05%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -8,6 +8,10 @@ var search = function(nums, target) {
     let start = 0;
     let end = nums.length-1;
     let mid;
+    if (nums.length === 0) return -1;
+    if (nums.length === 1) return nums[0] === target ? 0 : -1;
+    if (target < nums[0] || target > nums[end]) return -1;
+    
     while(start<=end){
         mid = Math.floor((start+end)/2)
         
