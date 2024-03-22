14dba49dba6a2d95fee3326a1a4be02b68338431
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-03-23 00:34:36 +0530

    Time: 69 ms (5.06%), Space: 52.5 MB (22.84%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -8,9 +8,10 @@ var search = function(nums, target) {
     let start = 0;
     let end = nums.length-1;
     let mid;
-    if (nums.length === 0) return -1;
-    if (nums.length === 1) return nums[0] === target ? 0 : -1;
-    if (target < nums[0] || target > nums[end]) return -1;
+    
+    if (nums.length === 0) return -1; // if array is empty
+    if (nums.length === 1) return nums[0] === target ? 0 : -1; // if array has single element
+    if (target < nums[0] || target > nums[end]) return -1; // if target is not bound in the array
     
     while(start<=end){
         mid = Math.floor((start+end)/2)
