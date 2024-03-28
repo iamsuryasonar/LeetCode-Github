838d64280165fface3ce701eb5705882de54afe6
Surya Sonar <79869026+iamsuryasonar@users.noreply.github.com>
2024-03-28 19:21:07 +0530

    Time: 85 ms (91.91%), Space: 57.9 MB (96.00%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -48,23 +48,3 @@ var findKthLargest = function (nums, k) {
     quickSelect(nums, 0, nums.length - 1)
     return nums[mid]
 };
-
-// var findKthLargest = function (nums, k) {
-//     let n = nums.length;
-//     let mid;
-
-//     function quickSelect(arr, low, high) {
-//         if (low <= high) { // Change here: <= instead of <
-//             mid = partition(arr, low, high);
-//             if (mid + 1 === (n - k + 1)) {
-//                 return arr[mid];
-//             } else if (mid + 1 < (n - k + 1)) {
-//                 return quickSelect(arr, mid + 1, high);
-//             } else {
-//                 return quickSelect(arr, low, mid - 1);
-//             }
-//         }
-//     }
-
-//     return quickSelect(nums, 0, nums.length - 1);
-// };
